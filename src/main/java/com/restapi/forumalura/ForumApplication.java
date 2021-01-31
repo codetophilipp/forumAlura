639fa40a1462f70ead454b017ee21fbbacf6f331
Phil <codetophilipp@gmail.com>
2021-01-31 16:16:09 -0300

    envio controller inicial e classes modelo

---
+++ src/main/java/com/restapi/forumalura/ForumApplication.java
@@ -0,0 +1,17 @@
+package com.restapi.forumalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
+
+// ctrl n = new class / interface
+// ctrl shft o = imports
+// ctrl shft f = format
